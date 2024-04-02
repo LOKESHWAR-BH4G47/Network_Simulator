08546d8ecc8ace2db5ece7bb090a60046d779a5a
LOKESHWAR-BH4G47 <bhagatlokeshwar7@proton.me>
2024-04-03 02:20:23 +0530

    changed device.py and main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from devices import *
+
+def main():
+    n_devices = int(input("enter no of devices : "))
+
+    hub = Hub("CentralHub")
+    devices = [Device(f"Device{i}") for i in range(1, n_devices + 1)]
+ 
+    for device in devices:
+        hub.connect(device)
+
+    s = int(input("enter the device number who want to send:  "))
+    r = int(input("enter the device number you want to send:  "))
+ 
+    devices[s-1].send_data("10101", hub, devices[r-1])
+
+if __name__ == "__main__":
+    main()
